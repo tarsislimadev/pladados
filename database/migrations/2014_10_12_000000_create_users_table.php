a91de2951241118f019056a5f8b652d13937727f
Tarsis de Lima <tarsis.mikael@hotmail.com>
2017-09-21 17:31:16 -0300

    Alterando migração da tabela de usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('lastname');
+            $table->string('cpf');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
