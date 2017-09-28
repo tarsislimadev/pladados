dc1bbe7036cf9a99f56b9ebb570b60cee1536051
Tarsis de Lima <tarsis.mikael@hotmail.com>
2017-09-28 00:31:15 -0300

    Adicionando helper de terceiro para visualização de grades

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,13 +14,14 @@ let mix = require('laravel-mix');
 mix.scripts([
     'resources/assets/js/vendor/jq.js', 
     'resources/assets/js/vendor/ko.js', 
+    'resources/assets/js/vendor/ko/simpleGrid.js', 
     'resources/assets/js/vendor/bs.js', 
     'resources/assets/js/vendor/promise.js', 
     'resources/assets/js/helper/string.js',
     'resources/assets/js/helper/file.js',
     'resources/assets/js/helper/ajax.js',
     'resources/assets/js/helper/ajax/upload.js',
-    'resources/assets/js/helper/ajax/loadsSave.js',
+    'resources/assets/js/helper/ajax/*.js',
     'resources/assets/js/app.js'
 ], 'public/js/app.js')
    .sass('resources/assets/sass/app.scss', 'public/css');
