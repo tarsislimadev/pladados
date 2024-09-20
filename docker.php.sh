dd7e13dd1ecbdd117121ccac9afe03108df660ab
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-20 12:03:34 -0300

    Updating app files

---
+++ docker.php.sh
@@ -0,0 +1,1 @@
+docker run --rm -it --net host -v /media/user/bdd1dc97-238a-4f44-b1ac-90363bfd5d11/Projects/github.com/pladados:/app -w /app php:7-alpine sh 
---
+++ require.laravel.sh
@@ -0,0 +1,5 @@
+
+
+composer require laravel/framework:6.20.44 egulias/email-validator:^2.1.10 nesbot/carbon:^2.31 symfony/console:^4.3.4 symfony/debug:^4.3.4 symfony/finder:^4.3.4 
+
+
